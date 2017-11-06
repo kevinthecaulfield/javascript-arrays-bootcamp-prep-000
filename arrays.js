bc69c997f70baa35c36d6b42963c93d99e861841
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-06 21:44:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  [array.slice(array)]
+  [array.slice(0)]
   return array
 }
