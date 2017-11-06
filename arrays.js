655b572ddcc9f9bcfcd36e18ef95908bedead050
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-06 21:48:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,6 +42,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  [array.slice(removeElementFromEndOfArray.length - 1)]
+  [array.slice(- 1)]
   return array
 }
