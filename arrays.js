5fa8ae7682523989c7cf2f2d14d477cae14deac8
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-06 21:42:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,10 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  [array.shift(array)]
+  return array
+}
+
+
